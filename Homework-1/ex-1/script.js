2b956676e53e5e165a0ffc6e829759e27444159b
vladsrka <svv.post01@gmail.com>
2024-07-31 21:48:38 +0300

    work

--- Homework-1/ex-1/script.js
+++ Homework-1/ex-1/script.js
@@ -1,42 +1,68 @@
-function caesarCipher(text, shift) {
-  const alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-  const alphabetLength = alphabet.length;
-  let result = "";
+// EX 1.1
+// function caesarCipher(text, shift) {
+//   const alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+//   const alphabetLength = alphabet.length;
+//   let result = "";
 
-  // Приводим сдвиг к диапазону алфавита
-  shift = shift % alphabetLength;
+//   // Приводим сдвиг к диапазону алфавита
+//   shift = shift % alphabetLength;
 
-  for (let i = 0; i < text.length; i++) {
-    const char = text[i];
-    const isUpperCase = char === char.toUpperCase();
+//   for (let i = 0; i < text.length; i++) {
+//     const char = text[i];
+//     const isUpperCase = char === char.toUpperCase();
 
-    if (alphabet.includes(char.toLowerCase())) {
-      const currentIndex = alphabet.indexOf(char.toLowerCase());
-      const newIndex = (currentIndex + shift + alphabetLength) % alphabetLength; // Сдвиг с учетом длины алфавита
-      const newChar = alphabet[newIndex];
+//     if (alphabet.includes(char.toLowerCase())) {
+//       const currentIndex = alphabet.indexOf(char.toLowerCase());
+//       const newIndex = (currentIndex + shift + alphabetLength) % alphabetLength; // Сдвиг с учетом длины алфавита
+//       const newChar = alphabet[newIndex];
 
-      result += isUpperCase ? newChar.toUpperCase() : newChar; // Восстанавливаем регистр
-    } else {
-      result += char; // Не шифруем символы, не входящие в алфавит
-    }
-  }
+//       result += isUpperCase ? newChar.toUpperCase() : newChar; // Восстанавливаем регистр
+//     } else {
+//       result += char; // Не шифруем символы, не входящие в алфавит
+//     }
+//   }
 
-  return result;
+//   return result;
+// }
+
+// function encrypt(text, shift) {
+//   return caesarCipher(text, shift);
+// }
+
+// function decrypt(text, shift) {
+//   return caesarCipher(text, -shift);
+// }
+
+// const originalText = "Привет, Мир!";
+// const shift = 3;
+
+// const encryptedText = encrypt(originalText, shift);
+// console.log("Зашифрованный текст:", encryptedText);
+
+// const decryptedText = decrypt(encryptedText, shift);
+// console.log("Дешифрованный текст:", decryptedText);
+
+// EX 1.2
+// Функция для шифрования текста с использованием AES
+function aesEncrypt(plainText, secretKey) {
+  const encrypted = CryptoJS.AES.encrypt(plainText, secretKey).toString();
+  return encrypted;
 }
 
-function encrypt(text, shift) {
-  return caesarCipher(text, shift);
+// Функция для дешифрования текста с использованием AES
+function aesDecrypt(cipherText, secretKey) {
+  const decrypted = CryptoJS.AES.decrypt(cipherText, secretKey);
+  return decrypted.toString(CryptoJS.enc.Utf8);
 }
 
-function decrypt(text, shift) {
-  return caesarCipher(text, -shift);
-}
+// Пример использования
+const secretKey = "mySecretKey123"; // Ваш секретный ключ
+const textToEncrypt = "Hello, World!";
 
-const originalText = "Привет, Мир!";
-const shift = 3;
-
-const encryptedText = encrypt(originalText, shift);
+// Шифруем текст
+const encryptedText = aesEncrypt(textToEncrypt, secretKey);
 console.log("Зашифрованный текст:", encryptedText);
 
-const decryptedText = decrypt(encryptedText, shift);
+// Дешифруем текст
+const decryptedText = aesDecrypt(encryptedText, secretKey);
 console.log("Дешифрованный текст:", decryptedText);
